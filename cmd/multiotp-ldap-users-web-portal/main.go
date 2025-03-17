b8d627a135f7ddd2f17e35a257fa6767a8e43007
slayerjk <slayer.jk@mail.ru>
2025-03-17 14:14:07 +0500

    + written multiOTP functions(cmd): del user, resync users, get totpURL

--- cmd/multiotp-ldap-users-web-portal/main.go
+++ cmd/multiotp-ldap-users-web-portal/main.go
@@ -18,22 +18,36 @@ func main() {
 
 	user := "marchenm"
 
-	// base32 encoded seed
-	// seed := "J6E2I53UDD6WRN7BHFC5DH7EX47VK2TO"
+	// TEST ONLY!!!
+	// del multiOTP user
+	err := multiotp.DelMultiOTPUser(user, *multiOTPBinPath)
+	if err != nil {
+		fmt.Printf("failed to del user:\n\t%v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s deleted or doesn't exist\n", user)
+
+	// resync multiOTP users
+	err = multiotp.ResyncMultiOTPUsers(*multiOTPBinPath)
+	if err != nil {
+		fmt.Printf("failed to resync users:\n\t%v", err)
+		os.Exit(1)
+	}
+	fmt.Println("successfully resynced users")
 
 	// get TOTP URL for user
-	totpURL, err := multiotp.GetMultiotpTokenURL(user, *multiOTPBinPath)
+	totpURL, err := multiotp.GetMultiOTPTokenURL(user, *multiOTPBinPath)
 	if err != nil {
 		fmt.Printf("failed to get totpURL for %s:\n\t%v", user, err)
 		os.Exit(1)
 	}
+	fmt.Printf("got totpURL for %s\n", user)
 
 	// generate QR SVG
-	svgFilePath, err := qrwork.GenerateTOTPSvgQR(totpURL, ".", "TEST")
+	_, err = qrwork.GenerateTOTPSvgQR(totpURL, ".", "TEST")
 	if err != nil {
 		fmt.Printf("failed to gen QR:\n\t%v", err)
 		os.Exit(1)
 	}
-	fmt.Printf("DONE: result is: %s", svgFilePath)
-
+	fmt.Printf("QR for %s is successfully generated\n", user)
 }
--- internal/multiotp/multiotp.go
+++ internal/multiotp/multiotp.go
@@ -11,14 +11,33 @@ multiotp -qrcode user png_file_name.png
 multiotp -update-pin user pin
 multiotp -remove-token user
 
+# reissue token
+1) multiotp -delete user
+2) multiotp -ldap-users-sync
+
+# get totpURL
 multiotp -urllink user
 # otpauth://totp/multiOTP:<NAME>%20<SURNANME>?secret=<BASE32 SEED>&digits=6&period=30
 */
 
-func GetMultiotpTokenURL(user string, multiOTPBinPath string) ([]byte, error) {
+// Get MultiOTP user's totpURL
+func GetMultiOTPTokenURL(user string, multiOTPBinPath string) ([]byte, error) {
 	// define command to get TOTP URL for user
 	cmd := exec.Command(multiOTPBinPath, "-urllink", user)
-	out, _ := cmd.Output()
+	// due to multiotp console tools throw Exit codes every time
+	// need to check err.ExitCode, because err will be always
+	// exit status 17: is success for '-urllink' cmd
+	out, err := cmd.Output()
+	if err, ok := err.(*exec.ExitError); ok {
+		// 17 INFO: UrlLink successfully created
+		// 21 ERROR: User doesn't exist
+		switch {
+		case err.ExitCode() == 21:
+			return nil, fmt.Errorf("%s doesn't exist", user)
+		case err.ExitCode() != 17:
+			return nil, err
+		}
+	}
 
 	// check output is what expected
 	pattern := regexp.MustCompile(`^otpauth:`)
@@ -26,4 +45,46 @@ func GetMultiotpTokenURL(user string, multiOTPBinPath string) ([]byte, error) {
 		return nil, fmt.Errorf("mutliotp command doesn't match '^otpauth://', output:\n\t\t%s", out)
 	}
 	return out, nil
+}
+
+// Delete MultiOTP User
+// If user doesn't exist - returns noting(not error)
+func DelMultiOTPUser(user string, multiOTPBinPath string) error {
+	// define command to delete user
+	cmd := exec.Command(multiOTPBinPath, "-delete", user)
+	// due to multiotp console tools throw Exit codes every time
+	// need to check err.ExitCode, because err will be always
+	// 12 INFO: User successfully deleted: is success for '-delete user' cmd
+	// OR
+	// 19 INFO: Requested operation successfully done: is success for '-delete user' cmd
+	// 21 ERROR: User doesn't exist: not error
+	_, err := cmd.Output()
+	if err, ok := err.(*exec.ExitError); ok {
+		switch {
+		case err.ExitCode() == 21:
+			return nil
+		case err.ExitCode() != 12 && err.ExitCode() != 19:
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Resync MultiOTP Users
+func ResyncMultiOTPUsers(multiOTPBinPath string) error {
+	// define command to delete user
+	cmd := exec.Command(multiOTPBinPath, "-ldap-users-sync")
+	// due to multiotp console tools throw Exit codes every time
+	// need to check err.ExitCode, because err will be always
+	// 19 INFO: Requested operation successfully done: is success for '-delete user' cmd
+	_, err := cmd.Output()
+
+	if err, ok := err.(*exec.ExitError); ok {
+		if err.ExitCode() != 19 {
+			return err
+		}
+	}
+
+	return nil
 }
